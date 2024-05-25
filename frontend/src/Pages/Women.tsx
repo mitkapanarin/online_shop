b3317593d7e529095436baf63d173f77b4775132
Mitka Panarin <mitka_j@yahoo.com>
2024-05-25 20:53:35 +0600

    interface and sample data added

--- frontend/src/Pages/Women.tsx
+++ frontend/src/Pages/Women.tsx
@@ -3,27 +3,29 @@ import ProductCard from "../components/Cards/ProductCard";
 import { IProductCardProps } from "../types/interface";
 
 export class Women extends Component {
-  data: IProductCardProps[] = [{
-    id: "1",
-    name: "White T-Shirt",
-    image:"./sample-image.jpg",
-    price: 25,
-    quantity: 10
-  },
-  {
-    id: "2",
-    name: "second White T-Shirt",
-    image:"./sample-image.jpg",
-    price: 25,
-    quantity: 10
-  },
-  {
-    id: "3",
-    name: "third White T-Shirt",
-    image:"./sample-image.jpg",
-    price: 25,
-    quantity: 10
-  }]
+  data: IProductCardProps[] = [
+    {
+      id: "1",
+      name: "White T-Shirt",
+      image: "./sample-image.jpg",
+      price: 25,
+      quantity: 10,
+    },
+    {
+      id: "2",
+      name: "second White T-Shirt",
+      image: "./sample-image.jpg",
+      price: 25,
+      quantity: 10,
+    },
+    {
+      id: "3",
+      name: "third White T-Shirt",
+      image: "./sample-image.jpg",
+      price: 25,
+      quantity: 10,
+    },
+  ];
 
   render() {
     return (
@@ -31,10 +33,8 @@ export class Women extends Component {
         <h1>Womens clothing</h1>
 
         <div className="grid grid-cols-3 gap-5">
-          {this.data?.map((product: IProductCardProps)=> (
-            <ProductCard
-            {...product}
-          />
+          {this.data?.map((product: IProductCardProps) => (
+            <ProductCard {...product} />
           ))}
         </div>
       </div>
