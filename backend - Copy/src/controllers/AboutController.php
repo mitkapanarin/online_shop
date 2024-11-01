e519b6a3c90a33e06fddcbf6cd96e1d20ef5cc97
mitkapanarin <mitka_j@yahoo.com>
2024-11-01 13:30:53 +0100

    .

--- backend - Copy/src/controllers/AboutController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class AboutController
-{
-  public function index()
-  {
-    echo 'About Us Page';
-  }
-}
--- backend - Copy/src/controllers/HomeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HomeController
-{
-  public function index()
-  {
-    echo 'Welcome to the Home Page!';
-  }
-}
