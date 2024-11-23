8208c8d05ca3a51f43e9749c0276759ba7b7f5c8
mitka <mitka_j@yahoo.com>
2024-11-23 18:40:42 +0600

    frontend boilerplate added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Provider } from "react-redux";
-import { persistedStore, store } from "./store/index.ts";
-import { PersistGate } from "redux-persist/integration/react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {App} from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistedStore}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
