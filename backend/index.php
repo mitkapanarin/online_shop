3ccaf9bb850ab27f4317b8a942b2e9d58b51ee92
mitka <mitka_j@yahoo.com>
2024-12-15 20:51:56 +0600

    .

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,7 @@
 <?php
 
+error_log("Request received: " . $_SERVER['REQUEST_METHOD'] . " " . $_SERVER['REQUEST_URI']);
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
@@ -7,7 +9,7 @@ require 'vendor/autoload.php';
 
 use FastRoute\RouteCollector;
 
-// Add the namespace for your controllers
+// Add the namespace for controllers
 spl_autoload_register(function ($class) {
   $file = __DIR__ . '/src/controllers/' . str_replace('\\', '/', $class) . '.php';
   if (file_exists($file)) {
@@ -25,6 +27,9 @@ $dispatcher = FastRoute\simpleDispatcher(function (RouteCollector $r) {
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
+// Debug logging
+error_log("Processing request: $httpMethod $uri");
+
 // Strip query string (?foo=bar) and decode URI
 if (false !== $pos = strpos($uri, '?')) {
   $uri = substr($uri, 0, $pos);
@@ -33,13 +38,18 @@ $uri = rawurldecode($uri);
 
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 
+// Debug logging
+error_log("Route info: " . json_encode($routeInfo));
+
 switch ($routeInfo[0]) {
   case FastRoute\Dispatcher::NOT_FOUND:
+    error_log("404 Not Found: $uri");
     http_response_code(404);
     echo '404 Not Found';
     break;
   case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
     $allowedMethods = $routeInfo[1];
+    error_log("405 Method Not Allowed: $httpMethod $uri. Allowed methods: " . implode(', ', $allowedMethods));
     http_response_code(405);
     echo '405 Method Not Allowed';
     break;
@@ -48,7 +58,6 @@ switch ($routeInfo[0]) {
     $vars = $routeInfo[2];
     [$class, $method] = explode('@', $handler);
 
-    // Debugging statement
     error_log("Dispatching to $class@$method with vars: " . json_encode($vars));
 
     if (class_exists($class) && method_exists($class, $method)) {
